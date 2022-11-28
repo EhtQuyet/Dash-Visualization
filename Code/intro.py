7104e4886f6fddb6ded74a67e687d11fc604a4f4
Quyet <ntq.145@gmail.com>
2022-11-28 17:22:14 +0700

    Init

---
+++ Code/intro.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import plotly.express as px
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+
+df = pd.read_csv('./data/data_test.csv')
+
+print(df)
